780226c3b344008baa5de77ecd3d447d856b8948
Igor Klepacki <neg4n@icloud.com>
2021-08-24 00:25:33 +0200

    Upload base source

---
+++ README.md
@@ -0,0 +1,69 @@
+<div align="center">
+  <h2>faviconize</h2>
+  <h4>Library and CLI for generating favicons in all formats</h4>
+  <br />
+</div>
+
+## Motivation
+
+I _(neg4n)_ wanted to create simpler and more intuitive replacement
+for existing project with the same purpose - [Real Favicon Generator](https://realfavicongenerator.net).
+RFG is really good project but sadly it works only online, its [API](https://github.com/RealFaviconGenerator/rfg-api) for Node.js makes HTTP requests under the hood instead of working locally and whole generator seems a little bit overcomplicated for me, personally.
+
+## Getting started
+
+**faviconize** is a CLI application and a library at once.
+
+Installing globally
+
+```shell
+npm i faviconize -g
+```
+
+or if you want limit scope to the project
+
+```shell
+npm i faviconize -D
+```
+
+_You will probably want to install it as devDependency :D_
+
+## Usage
+
+### Command line
+
+```
+faviconize <image-to-resize>
+```
+then follow self-explanatory terminal user interface.
+
+### Code
+
+```js
+import { faviconize } from 'faviconize'
+
+async function run() {
+  await faviconize('path/to/image.jpg')
+  // ... or with custom output icon types
+  await faviconize('path/to/other-image.jpg', [
+    'apple-touch-icon',
+    'msapplication-TileImage',
+  ])
+  // ... or with all icon types and custom output directory
+  await faviconize('path/to/another-image.jpg', null, '.')
+}
+
+run()
+```
+
+## TODO
+
+- [ ] `example/` project
+- [ ] Documentation
+- [ ] Configuration
+- [ ] Tests
+
+## Ending speech
+
+This project is licensed under the MIT license.
+All contributions are welcome.
---
+++ src/index.ts
@@ -0,0 +1,113 @@
+#!/usr/bin/env node
+
+import * as path from 'path'
+import { access, mkdir } from 'fs/promises'
+import chalk from 'chalk'
+import * as sharp from 'sharp'
+import * as inquirer from 'inquirer'
+
+type IconType = 'apple-touch-icon'
+
+const ICON_TYPE_SIZE = {
+  icon: [196, 160, 96, 32, 16],
+  'msapplication-TileImage': [70, 150, 310],
+  'apple-touch-icon': [57, 144, 72, 144, 60, 120, 76, 152],
+}
+
+export async function faviconize(
+  inputFilePath: string,
+  outputTypes?: IconType | IconType[],
+  outputDirectory?: string,
+) {
+  const types = normalizeOutputTypes()
+  const currentDirectory = path.join(process.cwd())
+  const outputPath = await createOrUseOutputPath()
+
+  for (const [type, edgeSizes] of Object.entries(ICON_TYPE_SIZE)) {
+    if (types.includes(type)) {
+      for (const edge of edgeSizes) {
+        const size = [edge, edge]
+
+        const inputPath = path.resolve(path.join(currentDirectory, inputFilePath))
+        const outputFile = path.join(outputPath, `${type}-${size.join('x')}.png`)
+
+        sharp(inputPath)
+          .resize(...size)
+          .toFile(outputFile)
+      }
+    }
+  }
+
+  function normalizeOutputTypes() {
+    if (outputTypes) {
+      if (outputTypes instanceof Array) {
+        return outputTypes.filter(
+          (type, index, original) => original.indexOf(type) === index,
+        )
+      } else {
+        return [outputTypes]
+      }
+    } else {
+      return Object.keys(ICON_TYPE_SIZE)
+    }
+  }
+
+  async function createOrUseOutputPath() {
+    const outputPath = path.resolve(
+      path.join(currentDirectory, outputDirectory || 'icons/'),
+    )
+
+    try {
+      await access(outputPath)
+    } catch (error) {
+      await mkdir(outputPath)
+    }
+
+    return outputPath
+  }
+}
+
+async function cli() {
+  const args = process.argv.slice(2, process.argv.length)
+  if (args.length !== 1) {
+    console.log(`
+    ${chalk.bgBlack(chalk.bold(chalk.red(` CLI Error: Incorrect number of arguments `)))}
+    ${chalk.white(
+      `${chalk.greenBright('Proper usage:')} faviconize ${chalk.gray('<path-to-image>')}`,
+    )}
+    `)
+    process.exit(1)
+  }
+
+  const [inputPath] = args
+  const { outputTypes, outputDirectory } = await cliInterface()
+
+  faviconize(inputPath, outputTypes, outputDirectory)
+
+  async function cliInterface(): Promise<{
+    outputTypes: IconType[]
+    outputDirectory: string
+  }> {
+    try {
+      return await inquirer.prompt([
+        {
+          name: 'outputTypes',
+          message: 'Choose output icon types:',
+          type: 'checkbox',
+          default: Object.keys(ICON_TYPE_SIZE),
+          choices: Object.keys(ICON_TYPE_SIZE),
+        },
+        {
+          name: 'outputDirectory',
+          message: 'Choose output directory:',
+          type: 'input',
+          default: 'icons/',
+        },
+      ])
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+cli()
