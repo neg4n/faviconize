973e0754ca401f8eb8254b581ba643bf38bae594
Igor Klepacki <neg4n@icloud.com>
2021-09-04 17:17:00 +0200

    Change module resolution

--- packages/faviconize/src/faviconize.ts
+++ packages/faviconize/src/faviconize.ts
@@ -1,5 +1,5 @@
 import * as path from 'path'
-import * as sharp from 'sharp'
+import sharp from 'sharp'
 
 import { iconTypesAndEdgesMap } from './constants'
 import { normalizeOutputTypes, resolveAndCreateOrUseOutputPath, resolveAndCheckInputFilePath } from './helpers'
