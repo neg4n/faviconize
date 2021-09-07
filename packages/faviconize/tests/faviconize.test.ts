0bc943a29e781fea0786ba9baf6cf2ccfb72accf
Igor Klepacki <neg4n@icloud.com>
2021-09-07 03:26:18 +0200

    Remove debug console log from tests and fix CircleCI coverage

--- packages/faviconize/tests/faviconize.test.ts
+++ packages/faviconize/tests/faviconize.test.ts
@@ -1,8 +1,7 @@
-import { faviconize, generateIconsLinkTags } from '../src/faviconize'
+import { faviconize } from '../src/faviconize'
 
 describe(faviconize, () => {
   it('is a function', async () => {
-    console.log(await generateIconsLinkTags(null, '#cccccc'))
     expect(typeof faviconize).toBe('function')
   })
 })
